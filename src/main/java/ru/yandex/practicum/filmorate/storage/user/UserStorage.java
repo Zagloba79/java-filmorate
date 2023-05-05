7f9704dbe09d1c2db2d9212f0c4f27b4d6a9efa4
Sirotin Alexandr <an9262778931@yandex.ru>
2023-05-05 15:45:06 +0300

    add-friends-likes

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -7,9 +7,14 @@ import java.util.Set;
 
 public interface UserStorage {
     List<User> findAll();
+
     User getUser(int id);
+
     List<User> getUsers(Set<Integer> ids);
+
     User create(User user);
+
     User update(User user);
+
     void delete(User user);
 }
